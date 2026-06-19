5bae1f66bb7d3fef422b83ff087a7a76c8dc6507
junlinpoo-hue <junlinpoo@gmail.com>
2026-06-19 09:37:52 +0800

    Update libwrt-6.12.sh

--- libwrt-6.12.sh
+++ libwrt-6.12.sh
@@ -7,9 +7,9 @@ cd "$OPENWRT_PATH" || exit 1
 echo "========================== 修复 hostapd 版本冲突 =========================="
 PATCH_FILE="package/network/services/hostapd/patches/900-hostapd-update-muedca-params.patch"
 if [ -f "$PATCH_FILE" ]; then
-    rm -f "$PATCH_FILE"
+    #rm -f "$PATCH_FILE"
     echo "已删除冲突补丁: $PATCH_FILE"
 else
-    find package/network/services/hostapd/ -name "*muedca*.patch" -exec rm -f {} \;
+    #find package/network/services/hostapd/ -name "*muedca*.patch" -exec rm -f {} \;
     echo "已清理所有 muedca 相关补丁"
 fi
